11c71a9a6a866b2c66e0c4b7cd438889b3bff00b
C0reta <161186212+C0reta@users.noreply.github.com>
2025-11-24 17:01:40 +0900

    lab03

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { Text, View, Button, TextInput } from 'react-native';
 import React, { useState } from 'react';
 import CalendarPicker from 'react-native-calendar-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
 
@@ -8,6 +9,8 @@ export default function App() {
   const [date1, setDate1] = useState('')
   const [expense, setExpense] = useState('')
   const [memo, setMemo] = useState('')
+
+  const [selectedDate, setSelectedDate] = useState(null);
 
   var container = { flex: 1, paddingTop: 50 };
   var box = { marginTop: 10, marginHorizontal: 20 };
@@ -17,24 +20,50 @@ export default function App() {
   var expense_st = { flex: 1, borderBottomWidth: 1, marginHorizontal: 10, fontSize: 16, color: 'blue' };
   var memo_st = { borderBottomWidth: 1, fontSize: 16, marginTop: 10, paddingBottom: 10, color: 'blue' };
 
-  function onDateChange(d) {
-    console.log(d.getFullYear() + " " + (d.getMonth() + 1) + " " + d.getDate());
-    // getMonth: 0~11 getDate: 날짜 그대로
-    setDate(d.getFullYear() + " " + (d.getMonth() + 1) + " " + d.getDate());
-    // day month date year
+  async function onDateChange(dt) {
+    if (!dt) return;
+    const d = dt.toDate();
+    console.log(d.getFullYear(), d.getMonth() + 1, d.getDate());
+
+    var key = d.getFullYear().toString() + (d.getMonth() + 1).toString() + d.getDate().toString();
+
+    setDate(key);
     setDate1(d.toDateString());
+    setSelectedDate(d);
+
+    var value = await AsyncStorage.getItem(key);
+    var value_m = await AsyncStorage.getItem(key + "m");
+
+    console.log(value);
+
+
+    setExpense(value !== null ? value : '');
+    setMemo(value_m !== null ? value_m : '');
+  }
+
+  async function save_memo() {
+    if (date === '') {
+      alert("날짜를 선택해 주세요");
+      return;
+    } try {
+      await AsyncStorage.setItem(date, expense);
+      await AsyncStorage.setItem(date + 'm', memo);
+    } catch (e) {
+      console.error(e);
+    }
+
   }
   return (
     <View style={container}>
-      <CalendarPicker onDateChange={onDateChange} />
+      <CalendarPicker onDateChange={onDateChange} selectedStartDate={selectedDate} />
       <View style={box}>
         <Text style={datetext}> {date1}</Text>
         <View style={row_st}>
           <Text style={text}>Expense</Text>
-          <TextInput style={expense_st} keyboardType='numeric' onChangeText={setExpense} />
-          <Button title="SAVE" />
+          <TextInput style={expense_st} keyboardType='numeric' onChangeText={setExpense} value={expense} />
+          <Button title="SAVE" onPress={save_memo} />
         </View>
-        <TextInput style={memo_st} placeholder='Memo' onChangeText={setMemo} />
+        <TextInput style={memo_st} placeholder='Memo' onChangeText={setMemo} value={memo} />
       </View>
     </View>
   );
