ae02e9f68c9f4bdae9b2ee664190443203f040c4
C0reta <161186212+C0reta@users.noreply.github.com>
2025-11-24 16:07:05 +0900

    lab2. add memoui

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
+import { Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import CalendarPicker from 'react-native-calendar-picker';
 
 export default function App() {
 
   const [date, setDate] = useState('')
   const [date1, setDate1] = useState('')
+  const [expense, setExpense] = useState('')
+  const [memo, setMemo] = useState('')
 
   var container = { flex: 1, paddingTop: 50 };
-  var box = { margin: 20 };
-  var text = { fontSize: 20, marginVertical: 10 };
+  var box = { marginTop: 10, marginHorizontal: 20 };
+  var text = { fontSize: 16, marginVertical: 10 };
+  var datetext = { fontSize: 16, marginVertical: 10 };
+  var row_st = { flexDirection: 'row', alignItems: 'center' };
+  var expense_st = { flex: 1, borderBottomWidth: 1, marginHorizontal: 10, fontSize: 16, color: 'blue' };
+  var memo_st = { borderBottomWidth: 1, fontSize: 16, marginTop: 10, paddingBottom: 10, color: 'blue' };
 
   function onDateChange(d) {
-    console.log(d);
-    console.log(d.toString());
-    console.log(d.getFullYear(), d.getMonth(), d.getDate());
-    console.log(d.toDateString());
-    console.log(d.toLocaleString());
-
+    console.log(d.getFullYear() + " " + (d.getMonth() + 1) + " " + d.getDate());
     // getMonth: 0~11 getDate: 날짜 그대로
     setDate(d.getFullYear() + " " + (d.getMonth() + 1) + " " + d.getDate());
     // day month date year
@@ -28,8 +28,13 @@ export default function App() {
     <View style={container}>
       <CalendarPicker onDateChange={onDateChange} />
       <View style={box}>
-        <Text>Date: {date}</Text>
-        <Text style={text}> {date1}</Text>
+        <Text style={datetext}> {date1}</Text>
+        <View style={row_st}>
+          <Text style={text}>Expense</Text>
+          <TextInput style={expense_st} keyboardType='numeric' onChangeText={setExpense} />
+          <Button title="SAVE" />
+        </View>
+        <TextInput style={memo_st} placeholder='Memo' onChangeText={setMemo} />
       </View>
     </View>
   );
